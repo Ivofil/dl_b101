0565f7a2270b08839948d01f0d7e93335abf624b
Ivofil <63012264+Ivofil@users.noreply.github.com>
2020-04-17 16:19:03 +0400

    Add files via upload

---
+++ hw05_normal.py
@@ -0,0 +1,151 @@
+# Задание-1:
+# Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+import re
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
+       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
+       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
+       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
+       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'\
+       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'\
+       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'\
+       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'\
+       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'\
+       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'\
+       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'\
+       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
+       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
+       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+
+pattern = re.compile('[a-z]+')
+print(f'Через Re: \n{pattern.findall(line)}\n')
+
+
+alf = list(map(lambda x: chr(x), list(range(65,91))))
+line_lst = list(line)
+for i, elm in enumerate(line_lst[:]):
+    if elm in alf:
+        line_lst[i] = ' '
+ 
+line_str = ''.join(line_lst).split(' ')
+line_str_2 = [i for i in line_str if i != '']
+print(f'Без Re: \n{line_str_2}\n')
+
+
+# Задание-2:
+# Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки 
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+
+line_2T = "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
+       'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
+       'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
+       'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV'\
+       'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW'\
+       'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC'\
+       'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR'\
+       'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm'\
+       'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn'\
+       'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS'\
+       'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf'\
+       'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH'\
+       'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
+       'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
+       'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+
+pattern2 = re.compile('[a-z]{2,2}([A-Z]+)[A-Z]{2,2}')
+print(f'Через Re: \n{pattern2.findall(line_2)}\n')
+#print(f'Через Re: \n{pattern2.findall(line_2T)}\n')
+
+
+alf_BIG = list(map(lambda x: chr(x), list(range(65,91))))
+alf_SML = list(map(lambda x: chr(x), list(range(97,123))))
+line_lst2 = list(line_2)
+line_lst2r = []
+line_o = []
+oO = ' '
+count = 0
+
+for i, elm in enumerate(line_lst2[:]):
+    if elm in alf_BIG:
+        count += 1
+    elif count > 2:
+        count_s = 0
+        O = ''
+        for buk in oO[1:-2]:
+            if buk in alf_SML:
+                count_s += 1
+            elif count_s > 1:
+                O += buk
+        oO = ' '   
+        count = 0
+        if O != '':
+            line_lst2r.append(O)
+    elif oO[-1] in alf_BIG:
+        count = 0
+        oO = ' '
+    else:
+        count = 0
+    oO += elm    
+print(f'Без Re: \n{line_lst2r}\n')
+
+
+
+
+
+# Задание-3:
+# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
+# произвольными целыми цифрами, в результате в файле должно быть
+# 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр
+# в вышезаполненном файле.
+
+import random
+ 
+lst_rnd = [random.randint(0,9) for _ in range(2500)]
+lst_u = ''.join(list(map(lambda x: str(x), lst_rnd)))
+
+path = 'data\\' + '2500' + '.txt'
+with open(path, 'w', encoding='UTF-8') as file:
+    file.write(str(lst_u))
+with open(path, 'r', encoding='UTF-8') as file:
+    lst_d = str(file.read())
+print(lst_d)
+count_max = 0
+num_max = 0
+count_max = 0
+for num in range(10):
+    count_n = 0
+    for i, smv in enumerate(lst_d[:]):
+        if int(smv) == num:
+            count_n += 1
+        elif count_n > count_max:
+            count_max = count_n
+            num_max = num 
+            count_n = 0
+        else:
+            count_n = 0
+print(f'Самая длинная последовательность одинаковых цифр: {str(num_max) * count_max}')
+
+            
+            
+    
+    
+
+
+
+
+
+
+
+
+
